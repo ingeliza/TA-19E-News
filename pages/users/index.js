8060a47dbecb4172f53d657d15e1b046fb5b2e13
Martin Makejev <martin.makejev@tptlive.ee>
2022-02-26 01:23:04 +0200

    Fetching now works. Need to fix URL. URL supposed to be .../api/v1/users.

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,60 +1,21 @@
+import React from "react";
+import useSWR from "swr";
 
-    // const axios = require('axios');
+  const fetcher = (url) => fetch(url).then((res) => res.json());
 
-    //     axios.get('https://tptusers.vercel.app/api/v1/students?email=artur.kaasik@tptlive.ee').then(resp => {
-    //         console.log(resp.data);
-    //     });
+export default function App() {
+  const { data, error } = useSWR(
+    "https://users-5otjipfax-tptusers.vercel.app/api/v1/students?email=artur.kaasik@tptlive.ee",
+    fetcher,
+  );
 
-
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-
-function App() {
-    const [isLoading, setLoading] = useState(true);
-    const [student, setStudent] = useState();
-
-    let config = {
-      headers: {'Access-Control-Allow-Origin' : '*'}
-    }
-
-    useEffect(() => {
-      axios.get('https://tptusers.vercel.app/api/v1/students?email=artur.kaasik@tptlive.ee', {headers: {"Access-Control-Allow-Origin": "*"} }).then(resp => {
-        setStudent(resp.data);
-        setLoading(false);
-      });
-    }, []);
-  
-    if (isLoading) {
-      return <div className="App">Loading...</div>;
-    }
-  
-    return (
-      <div className="App">
-        <h3>{student.firstname} {student.lastname}</h3>
-      </div>
-    );
-  }
-
-export default App;
-
-
-// import React from "react";
-// import useSWR from "swr";
-
-//   const fetcher = (url) => fetch(url).then((res) => res.json());
-
-// export default function App() {
-//   const { data, error } = useSWR(
-//     "https://tptusers.vercel.app/api/v1/students?email=artur.kaasik@tptlive.ee",
-//     fetcher,
-//   );
-
-//   if (error) return "An error has occurred.";
-//   if (!data) return "Loading...";
-//   return (
-//     <div>
-//       <p>{data.firstname} {data.lastname}</p>
-//     </div>
-//   );
-// }
-
+  if (error) return "An error has occurred.";
+  if (!data) return "Loading...";
+  return (
+    <div>
+      <h3>Id : {data.id}</h3>
+      <p>Firstname : {data.firstname}</p>
+      <p>Lastname : {data.lastname}</p>
+    </div>
+  );
+}
