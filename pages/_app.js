63bbf0e74cec085a3476fad720ba13cabfc993cf
iSamsungS <32791265+iSamsungS@users.noreply.github.com>
2022-03-16 02:58:17 +0200

    authentication

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/globals.css'
 import 'antd/dist/antd.css'
+import { UserProvider } from '@auth0/nextjs-auth0';
 import { ReactDOM } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+    <Component {...pageProps} />
+    </UserProvider>
+  );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -7,19 +7,47 @@ import Login from './login-panel'
 import { handler } from "./api/v1/login"
 import Navbar from '../resources/components/main_layout'
 import MainLayout from '../resources/components/main_layout'
+import Link from 'next/link'
+import { useUser } from '@auth0/nextjs-auth0'
 
 
 
 export default function Home() {
+<<<<<<< Updated upstream
   //const [content, setContent] = useState([]);
   const content = [{title: "Cat1", src:"pages/placeholder.jpeg"},{title: "Cat2", src:"placeholder.jpg"},{title: "Cat3", src:"placeholder.jpg"}]
 
   /*useEffect(function() {
     fetch("").then(res => res.json()).then(data => setContent(data))
   }, []) */
+=======
+  const{user}=useUser();
+  console.log(user);
+>>>>>>> Stashed changes
 
   return (
+    
     <div className={styles.container}>
+<<<<<<< Updated upstream
+=======
+      <nav>
+      {!user && (
+        <Link href="/api/auth/login">
+          <a>Logi sisse</a>
+        </Link>
+      )}
+
+      {user && (
+        <>
+        <img src={user.picture} alt={user.name}/>
+        <Link href="/api/auth/logout">
+          <a>Logi välja</a>
+        </Link>
+        </>
+        )}
+      </nav>
+
+>>>>>>> Stashed changes
         <MainLayout/>
     </div>
     
